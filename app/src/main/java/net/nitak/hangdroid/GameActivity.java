aa26c0251a2787fd0b8388e6e4f420e901b9da8c
Qasim Al Khuzaie <qrasool@gmail.com>
2015-12-03 16:49:19 +0300

    Letter input is logged upon confirming the choice of the letter

--- app/src/main/java/net/nitak/hangdroid/GameActivity.java
+++ app/src/main/java/net/nitak/hangdroid/GameActivity.java
@@ -2,15 +2,30 @@ package net.nitak.hangdroid;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class GameActivity extends Activity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
     }
 
+    public void introduceLetter(View v){
+        EditText myEditText = (EditText) findViewById(R.id.editTextLetter);
 
+        String letter = myEditText.getText().toString();
+
+        Log.d("MYLOG", "The letter is "+letter);
+
+
+    }
 
 }
