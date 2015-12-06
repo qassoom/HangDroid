dfc3dceba995ba19c706b27a68e1eea8e1ae8072
Qasim Al Khuzaie <qrasool@gmail.com>
2015-12-06 11:33:04 +0300

    Toast if no letter was input

--- app/src/main/java/net/nitak/hangdroid/GameActivity.java
+++ app/src/main/java/net/nitak/hangdroid/GameActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class GameActivity extends Activity {
 
@@ -25,7 +26,18 @@ public class GameActivity extends Activity {
 
         Log.d("MYLOG", "The letter is "+letter);
 
+        if (letter.length() == 1) {
+            checkLetter(letter);
+        } else {
+            Toast.makeText(this, //where
+                    "Please Introduce Letter", //what
+                    Toast.LENGTH_SHORT).show(); //how
+        }
 
     }
 
+
+    public void checkLetter(String introducedLetter){
+
+    }
 }
